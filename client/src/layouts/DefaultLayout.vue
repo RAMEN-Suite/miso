7f600c8b3697e2db28b4ffed51fe8531aabd2813
Maximilian Michel <maxi.michel@gmx.net>
2026-08-18 13:25:49 +0200

    Remove footer from app for now

--- client/src/layouts/DefaultLayout.vue
+++ client/src/layouts/DefaultLayout.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import TopBar from "../components/TopBar.vue";
-import BottomBar from "../components/BottomBar.vue";
 </script>
 
 <template>
@@ -9,7 +8,6 @@ import BottomBar from "../components/BottomBar.vue";
     <main class="flex-grow-1 min-h-0 overflow-y-auto">
       <slot></slot>
     </main>
-    <BottomBar />
   </div>
 </template>
 
